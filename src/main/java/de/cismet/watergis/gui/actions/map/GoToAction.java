4899916323bf7f33a8397b11e52eec2135dab00e
Gilles Baatz <gilles.baatz@cismet.de>
2013-08-26 14:07:30 +0200

    #6 Implemented 'Goto xy'

--- src/main/java/de/cismet/watergis/gui/actions/map/GoToAction.java
+++ src/main/java/de/cismet/watergis/gui/actions/map/GoToAction.java
@@ -17,9 +17,19 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
+import de.cismet.cismap.commons.BoundingBox;
+import de.cismet.cismap.commons.Crs;
+import de.cismet.cismap.commons.XBoundingBox;
+import de.cismet.cismap.commons.gui.MappingComponent;
+import de.cismet.cismap.commons.interaction.CismapBroker;
+
+import de.cismet.tools.StaticDecimalTools;
+
 import de.cismet.watergis.broker.AppBroker;
+import de.cismet.watergis.broker.ComponentName;
 
 import de.cismet.watergis.gui.actions.selection.*;
 
@@ -62,10 +72,37 @@ public class GoToAction extends AbstractAction {
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        LOG.info("Not supported yet.");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Goto to position");
+        }
+        final MappingComponent mappingComponent = AppBroker.getInstance().getMappingComponent();
+        final XBoundingBox c = (XBoundingBox)mappingComponent.getCurrentBoundingBoxFromCamera();
+        final double x = (c.getX1() + c.getX2()) / 2;
+        final double y = (c.getY1() + c.getY2()) / 2;
+        final String s = JOptionPane.showInputDialog(
+                AppBroker.getInstance().getComponent(ComponentName.MAIN),
+                "Zentriere auf folgendem Punkt: x,y",
+                StaticDecimalTools.round(x)
+                        + ","
+                        + StaticDecimalTools.round(y));
+        try {
+            final String[] sa = s.split(",");
+            final Double gotoX = new Double(sa[0]);
+            final Double gotoY = new Double(sa[1]);
+            final String currentCrsCode = CismapBroker.getInstance().getSrs().getCode();
+            final XBoundingBox bb = new XBoundingBox(
+                    gotoX,
+                    gotoY,
+                    gotoX,
+                    gotoY,
+                    currentCrsCode,
+                    mappingComponent.isInMetricSRS());
+            mappingComponent.gotoBoundingBox(bb, true, false, mappingComponent.getAnimationDuration());
+        } catch (Exception skip) {
+        }
     }
     @Override
     public boolean isEnabled() {
-        return false || AppBroker.getInstance().isActionsAlwaysEnabled();
+        return true || AppBroker.getInstance().isActionsAlwaysEnabled();
     }
 }
