fc40b4d86099ecfc10b2da78ec1518565057652a
Gilles Baatz <gilles.baatz@cismet.de>
2013-08-26 13:44:58 +0200

    #6 Saving projects works now

--- src/main/java/de/cismet/watergis/gui/actions/SaveProjectAction.java
+++ src/main/java/de/cismet/watergis/gui/actions/SaveProjectAction.java
@@ -16,11 +16,22 @@ import org.apache.log4j.Logger;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import java.io.File;
+
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileFilter;
+
+import de.cismet.cismap.commons.RestrictedFileSystemView;
+
+import de.cismet.cismap.navigatorplugin.CismapPlugin;
+
+import de.cismet.tools.configuration.ConfigurationManager;
 
 import de.cismet.watergis.broker.AppBroker;
+import de.cismet.watergis.broker.ComponentName;
 
 import de.cismet.watergis.gui.WatergisApp;
 
@@ -64,10 +75,60 @@ public class SaveProjectAction extends AbstractAction {
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        LOG.info("Not supported yet.");
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Save Project");
+        }
+        save();
     }
     @Override
     public boolean isEnabled() {
-        return false || AppBroker.getInstance().isActionsAlwaysEnabled();
+        return true || AppBroker.getInstance().isActionsAlwaysEnabled();
+    }
+
+    /**
+     * DOCUMENT ME!
+     */
+    private void save() {
+        JFileChooser fc;
+
+        try {
+            fc = new JFileChooser(WatergisApp.DIRECTORYPATH_WATERGIS);
+        } catch (Exception bug) {
+            // Bug Workaround http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6544857
+            fc = new JFileChooser(WatergisApp.DIRECTORYPATH_WATERGIS, new RestrictedFileSystemView());
+        }
+
+        fc.setFileFilter(new FileFilter() {
+
+                @Override
+                public boolean accept(final File f) {
+                    return f.isDirectory()
+                                || f.getName().toLowerCase().endsWith(".xml"); // NOI18N
+                }
+
+                @Override
+                public String getDescription() {
+                    return org.openide.util.NbBundle.getMessage(
+                            CismapPlugin.class,
+                            "CismapPlugin.mniSaveConfigActionPerformed.FileFilter.getDescription.return"); // NOI18N
+                }
+            });
+
+        final int state = fc.showSaveDialog(AppBroker.getInstance().getComponent(ComponentName.MAIN));
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("state:" + state); // NOI18N
+        }
+
+        final ConfigurationManager configurationManager = AppBroker.getConfigManager();
+        if (state == JFileChooser.APPROVE_OPTION) {
+            final File file = fc.getSelectedFile();
+            final String name = file.getAbsolutePath();
+
+            if (name.endsWith(".xml")) {                                // NOI18N
+                configurationManager.writeConfiguration(name);
+            } else {
+                configurationManager.writeConfiguration(name + ".xml"); // NOI18N
+            }
+        }
     }
 }
