9cdc809f17ae42f4ed6dc00228a95f7004f3035e
Gilles Baatz <gilles.baatz@cismet.de>
2013-08-27 09:16:03 +0200

    #6 added the 'Scale Manually' dialog

--- src/main/java/de/cismet/watergis/gui/actions/map/ScaleAction.java
+++ src/main/java/de/cismet/watergis/gui/actions/map/ScaleAction.java
@@ -17,7 +17,14 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+
+import de.cismet.cismap.commons.gui.MappingComponent;
+
+import de.cismet.cismap.navigatorplugin.CismapPlugin;
+
+import de.cismet.tools.gui.StaticSwingTools;
 
 import de.cismet.watergis.broker.AppBroker;
 
@@ -62,11 +69,24 @@ public class ScaleAction extends AbstractAction {
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        LOG.info("Not supported yet.");
+        final MappingComponent mappingComponent = AppBroker.getInstance().getMappingComponent();
+        try {
+            final String s = JOptionPane.showInputDialog(
+                    StaticSwingTools.getParentFrame(mappingComponent),
+                    org.openide.util.NbBundle.getMessage(
+                        ScaleAction.class,
+                        "ScaleAction.actionPerformed().InfoDialog.message"),
+                    ((int)mappingComponent.getScaleDenominator())
+                            + "");                               // NOI18N
+            final Integer i = new Integer(s);
+            mappingComponent.gotoBoundingBoxWithHistory(mappingComponent.getBoundingBoxFromScale(i));
+        } catch (Exception skip) {
+            LOG.error("Error in mniScaleActionPerformed", skip); // NOI18N
+        }
     }
 
     @Override
     public boolean isEnabled() {
-        return false || AppBroker.getInstance().isActionsAlwaysEnabled();
+        return true || AppBroker.getInstance().isActionsAlwaysEnabled();
     }
 }
