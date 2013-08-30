4a4b05242841f22a5723f1e754dd6921c6bebacf
Gilles Baatz <gilles.baatz@cismet.de>
2013-08-30 14:46:52 +0200

    jalopy

--- src/main/java/de/cismet/cids/custom/beans/watergis/Bookmark.java
+++ src/main/java/de/cismet/cids/custom/beans/watergis/Bookmark.java
@@ -1,3 +1,10 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -5,41 +12,76 @@
 package de.cismet.cids.custom.beans.watergis;
 
 import com.vividsolutions.jts.geom.Geometry;
+
 import de.cismet.cids.dynamics.CidsBean;
 
 /**
+ * DOCUMENT ME!
  *
- * @author Gilles Baatz
+ * @author   Gilles Baatz
+ * @version  $Revision$, $Date$
  */
-public class Bookmark extends CidsBean{
+public class Bookmark extends CidsBean {
+
+    //~ Instance fields --------------------------------------------------------
+
     private String name;
     private String description;
     private Geometry geom;
 
+    //~ Methods ----------------------------------------------------------------
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  name  DOCUMENT ME!
+     */
+    public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  description  DOCUMENT ME!
+     */
+    public void setDescription(final String description) {
         this.description = description;
     }
 
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
     public Geometry getGeom() {
         return geom;
     }
 
-    public void setGeom(Geometry geom) {
+    /**
+     * DOCUMENT ME!
+     *
+     * @param  geom  DOCUMENT ME!
+     */
+    public void setGeom(final Geometry geom) {
         this.geom = geom;
     }
-    
-    
-    
 }
