98781eb9015cc25dcb0bdcc0801a7645d48aed8f
Gilles Baatz <gilles.baatz@cismet.de>
2013-09-03 09:06:09 +0200

    #10 CreateBookmarkAction has a CreateBookmarkDialog as attribute and creates a Point feature in the middle of the map.

--- src/main/java/de/cismet/watergis/gui/actions/bookmarks/CreateBookmarkAction.java
+++ src/main/java/de/cismet/watergis/gui/actions/bookmarks/CreateBookmarkAction.java
@@ -11,17 +11,26 @@
  */
 package de.cismet.watergis.gui.actions.bookmarks;
 
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
 import org.apache.log4j.Logger;
 
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.ImageIcon;
 import javax.swing.KeyStroke;
+
+import de.cismet.cids.custom.beans.watergis.Bookmark;
+
+import de.cismet.cismap.commons.XBoundingBox;
+import de.cismet.cismap.commons.features.DefaultStyledFeature;
+import de.cismet.cismap.commons.features.Feature;
+import de.cismet.cismap.commons.gui.MappingComponent;
 
 import de.cismet.watergis.broker.AppBroker;
 
-import de.cismet.watergis.gui.actions.*;
+import de.cismet.watergis.gui.dialog.CreateBookmarkDialog;
 
 /**
  * DOCUMENT ME!
@@ -35,12 +44,18 @@ public class CreateBookmarkAction extends AbstractAction {
 
     private static final Logger LOG = Logger.getLogger(CreateBookmarkAction.class);
 
+    //~ Instance fields --------------------------------------------------------
+
+    private CreateBookmarkDialog bookmarkDialog;
+
     //~ Constructors -----------------------------------------------------------
 
     /**
      * Creates a new CreateBookmarkAction object.
+     *
+     * @param  bookmarkDialog  DOCUMENT ME!
      */
-    public CreateBookmarkAction() {
+    public CreateBookmarkAction(final CreateBookmarkDialog bookmarkDialog) {
         final String tooltip = org.openide.util.NbBundle.getMessage(
                 CreateBookmarkAction.class,
                 "CreateBookmarkAction.toolTipText");
@@ -53,16 +68,41 @@ public class CreateBookmarkAction extends AbstractAction {
                 CreateBookmarkAction.class,
                 "CreateBookmarkAction.mnemonic");
         putValue(MNEMONIC_KEY, KeyStroke.getKeyStroke(mnemonic).getKeyCode());
+
+        this.bookmarkDialog = bookmarkDialog;
     }
 
     //~ Methods ----------------------------------------------------------------
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        LOG.info("Not supported yet.");
+        final Geometry geom = createGeometrieInMiddleOfMap();
+        final Bookmark bookmark = new Bookmark();
+        bookmark.setGeometry(geom);
+        bookmark.setName(bookmarkDialog.getBookmarkName());
+        bookmark.setDescription(bookmarkDialog.getBookmarkDescription());
+        AppBroker.getInstance().getBookmarkManager().add(bookmark);
+        bookmarkDialog.dispose();
     }
+
     @Override
     public boolean isEnabled() {
-        return false || AppBroker.getInstance().isActionsAlwaysEnabled();
+        return true || AppBroker.getInstance().isActionsAlwaysEnabled();
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     */
+    private Geometry createGeometrieInMiddleOfMap() {
+        final MappingComponent mappingComponent = AppBroker.getInstance().getMappingComponent();
+        final XBoundingBox boundingBox = (XBoundingBox)mappingComponent.getCurrentBoundingBoxFromCamera();
+        final Point middleOfScreen = boundingBox.getGeometry().getCentroid();
+        final DefaultStyledFeature feature = new DefaultStyledFeature();
+        feature.setGeometry(middleOfScreen);
+        mappingComponent.addFeaturesToMap(new Feature[] { feature });
+
+        return middleOfScreen;
     }
 }
