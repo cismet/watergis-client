7be16d88141b856e8c43b858d330af1a99fdbad0
Gilles Baatz <gilles.baatz@cismet.de>
2013-09-03 15:15:44 +0200

    #10 override of hashCode() and equals(final Object obj) in Bookmark

--- src/main/java/de/cismet/cids/custom/beans/watergis/Bookmark.java
+++ src/main/java/de/cismet/cids/custom/beans/watergis/Bookmark.java
@@ -13,6 +13,8 @@ package de.cismet.cids.custom.beans.watergis;
 
 import com.vividsolutions.jts.geom.Geometry;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import de.cismet.cids.dynamics.CidsBean;
 
 /**
@@ -88,5 +90,29 @@ public class Bookmark extends CidsBean {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = (53 * hash) + ((this.name != null) ? this.name.hashCode() : 0);
+        hash = (53 * hash) + ((this.description != null) ? this.description.hashCode() : 0);
+        hash = (53 * hash) + ((this.geometry != null) ? this.geometry.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Bookmark other = (Bookmark)obj;
+        if ((this.name == null) ? (other.name != null) : (!this.name.equals(other.name))) {
+            return false;
+        }
+        return true;
     }
 }
