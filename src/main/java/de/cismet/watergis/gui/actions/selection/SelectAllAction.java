c4ad2ec6e1e93f88d45b78b52911754bed1e7aaf
therter <thorsten.herter@cismet.de>
2014-10-10 10:40:33 +0200

    #74 Introduction of the selection buttons, which consider all layers

--- src/main/java/de/cismet/watergis/gui/actions/selection/SelectAllAction.java
+++ src/main/java/de/cismet/watergis/gui/actions/selection/SelectAllAction.java
@@ -15,9 +15,23 @@ import org.apache.log4j.Logger;
 
 import java.awt.event.ActionEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 import javax.swing.KeyStroke;
+
+import de.cismet.cismap.commons.features.DefaultFeatureCollection;
+import de.cismet.cismap.commons.features.Feature;
+import de.cismet.cismap.commons.featureservice.AbstractFeatureService;
+import de.cismet.cismap.commons.gui.MappingComponent;
+import de.cismet.cismap.commons.gui.layerwidget.ActiveLayerModel;
+import de.cismet.cismap.commons.gui.piccolo.PFeature;
+import de.cismet.cismap.commons.gui.piccolo.eventlistener.SelectionListener;
+import de.cismet.cismap.commons.interaction.CismapBroker;
+import de.cismet.cismap.commons.rasterservice.MapService;
 
 import de.cismet.watergis.broker.AppBroker;
 
@@ -60,11 +74,38 @@ public class SelectAllAction extends AbstractAction {
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        LOG.info("Not supported yet.");
+        final ActiveLayerModel model = (ActiveLayerModel)AppBroker.getInstance().getMappingComponent()
+                    .getMappingModel();
+        final TreeMap<Integer, MapService> map = model.getMapServices();
+        final List<Feature> toBeSelected = new ArrayList<Feature>();
+
+        for (final MapService mapService : map.values()) {
+            if (mapService instanceof AbstractFeatureService) {
+                final AbstractFeatureService service = (AbstractFeatureService)mapService;
+                if (service.getPNode().getVisible()) {
+                    for (final Object featureObject : service.getPNode().getChildrenReference()) {
+                        final PFeature feature = (PFeature)featureObject;
+
+                        if (!feature.isSelected()) {
+                            feature.setSelected(true);
+                            final SelectionListener sl = (SelectionListener)CismapBroker.getInstance()
+                                        .getMappingComponent()
+                                        .getInputEventListener()
+                                        .get(MappingComponent.SELECT);
+                            sl.addSelectedFeature(feature);
+                            toBeSelected.add(feature.getFeature());
+                        }
+                    }
+                }
+            }
+        }
+
+        ((DefaultFeatureCollection)CismapBroker.getInstance().getMappingComponent().getFeatureCollection())
+                .addToSelection(toBeSelected);
     }
 
     @Override
     public boolean isEnabled() {
-        return false || AppBroker.getInstance().isActionsAlwaysEnabled();
+        return true || AppBroker.getInstance().isActionsAlwaysEnabled();
     }
 }
