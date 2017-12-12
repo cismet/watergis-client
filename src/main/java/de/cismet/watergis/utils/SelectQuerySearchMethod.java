982448ed375b1393674d30d76e67e28d514fc89c
therter <thorsten.herter@cismet.de>
2017-12-12 19:41:16 +0100

    #136 use the max feature count

--- src/main/java/de/cismet/watergis/utils/SelectQuerySearchMethod.java
+++ src/main/java/de/cismet/watergis/utils/SelectQuerySearchMethod.java
@@ -12,6 +12,9 @@ import org.apache.log4j.Logger;
 import org.openide.util.NbBundle;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.JOptionPane;
 
 import de.cismet.cids.search.QuerySearch;
 import de.cismet.cids.search.QuerySearchMethod;
@@ -23,6 +26,8 @@ import de.cismet.cismap.commons.featureservice.factory.FeatureFactory;
 import de.cismet.cismap.commons.util.SelectionManager;
 
 import de.cismet.commons.concurrency.CismetExecutors;
+
+import de.cismet.watergis.broker.AppBroker;
 
 /**
  * DOCUMENT ME!
@@ -121,6 +126,14 @@ public class SelectQuerySearchMethod implements QuerySearchMethod {
                     SelectionManager.getInstance()
                             .removeSelectedFeatures(SelectionManager.getInstance().getSelectedFeatures());
                 }
+            } catch (ExecutionException e) {
+                if (e.getCause() instanceof TooManyResultsException) {
+                    JOptionPane.showMessageDialog(AppBroker.getInstance().getWatergisApp(),
+                        e.getCause().getMessage(),
+                        "Hinweis",
+                        JOptionPane.WARNING_MESSAGE);
+                }
+                LOG.error("Error while selecting features", e);
             } catch (Exception e) {
                 LOG.error("Error while selecting features", e);
             }
